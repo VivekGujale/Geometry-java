628c89bfce7f589619d4dbe127f80662dc373feb
VivekGujale <vdgujale007@.com>
2022-10-06 20:16:07 +0530

    added logic to check two lines are equal or different

--- src/Geometry.java
+++ src/Geometry.java
@@ -4,12 +4,16 @@ public class Geometry {
     public static void main(String[] args) {
         System.out.println("Welcome to Geometry");
         Scanner obj = new Scanner(System.in);
-        System.out.println("Enter the values of x1,x2,y1,y2");
+        System.out.println("Enter the values of x1,y1,x2,y2");
         Integer x1 = obj.nextInt();
-        Integer x2 = obj.nextInt();
         Integer y1 = obj.nextInt();
+        Integer x2 = obj.nextInt();
         Integer y2 = obj.nextInt();
-        double LengthOfLine = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        System.out.println("Length of 2 points (" + x1 + "," + y1 + ") and (" + x2 + "," + y2 + ") =" + LengthOfLine);
+        //If result is true then two lines are equal.
+        //If result is false then two lines are not equal.
+        boolean result1 = x1.equals(y1);
+        System.out.println("Result is " + result1);
+        boolean result2 = x2.equals(y2);
+        System.out.println("Result is " + result2);
     }
 }
